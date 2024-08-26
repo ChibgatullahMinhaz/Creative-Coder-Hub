a9027514adfd3052aa0aff8e6ea2604bd0fe895c
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-08-26 22:08:35 +0600

    fixed

--- Expanding Card/script.js
+++ Expanding Card/script.js
@@ -1,11 +1,17 @@
 const Cards = document.querySelectorAll('.inner_container');
 
-Cards.forEach(card => {
-    card.addEventListener('click',()=>{
-        Cards.forEach(card =>{
-            card.classList.remove('active')
-        })
-        card.classList.add('active');
+const removeClass = ()=>{
+    Cards.forEach(card =>{
+        card.classList.remove("active")
 
-    } )
-});
+    })
+}
+const addClass = ()=>{
+    Cards.forEach(card =>{
+        card.addEventListener('click', ()=>{
+            removeClass()
+            card.classList.add("active")
+        })
+    })
+}
+addClass()
