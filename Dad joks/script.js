98442434768533e9fed34cfab370144f53beded2
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-02 02:19:39 +0600

    changes

--- Dad joks/script.js
+++ Dad joks/script.js
@@ -1,5 +1,26 @@
 const joke = document.querySelector('#joke');
-const jokeBtn = document.querySelector('#get-joke');
+const container = document.getElementById('container');
+
+// creating elements
+const headDiv = document.createElement('div');
+const h1 = document.createElement('h1');
+h1.textContent = 'Dad Joke Generator';
+headDiv.appendChild(h1);
+container.appendChild(headDiv)
+
+const jokeDiv = document.createElement('div');
+const p = document.createElement('p');
+p.textContent= "  What's Forrest Gump's computer password? 1forrest1";
+p.id= 'joke';
+jokeDiv.appendChild(p)
+container.appendChild(jokeDiv);
+
+const buttonDiv = document.createElement('div');
+const jokeBtn = document.createElement('button');
+jokeBtn.textContent= "Get Joke";
+jokeBtn.id= 'get-joke';
+buttonDiv.appendChild(jokeBtn);
+container.appendChild(buttonDiv)
 
 
 async function generateJoke(){
@@ -9,12 +30,7 @@ async function generateJoke(){
     const url = `https://api.api-ninjas.com/v1/dadjokes?limit=1`
     let dadjoke = await fetch(url, header);
     dadjoke = await dadjoke.json();
-    console.log(dadjoke,"dad joke");
-    console.log(dadjoke[0])
-    console.log(dadjoke[0].joke)
     joke.innerHTML = dadjoke[0].joke;
 
 }
-
-
 jokeBtn.addEventListener('click', generateJoke);
