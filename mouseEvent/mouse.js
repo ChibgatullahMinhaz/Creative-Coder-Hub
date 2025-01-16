ba6e135b8af28bc2e29691cb27a1d7658cef13af
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-01-16 20:33:40 +0600

    prpject set up

--- mouseEvent/mouse.js
+++
@@ -1,6 +0,0 @@
-const xposition = document.getElementById('xpos-value');
-const yposition = document.getElementById('ypos-value');
-window.addEventListener('mousemove', (event)=>{
-    xposition.textContent = event.clientX;
-    yposition.textContent = event.clientY;
-})
--- weightConverter/weight.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById('input');
-const output = document.getElementById('Pound');
-
-input.addEventListener('input', ()=>{
-    let val = input.value;
-    let out = val*2.2;
-    output.innerHTML = out.toFixed(2)
-})
